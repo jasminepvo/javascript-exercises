602fb486598236a8f439cb3500aa8e6c2381a30d
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-06-01 16:15:27 -0400

    Working Solution and passed 6 test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    // Swap values when min is greater than max using temporary holder
+    if(min>max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    // Return error for negative numbers
+    if(min<0 || max<0) return "ERROR";
 
+    // Return error if NOT number parameter
+    if(!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    
+    // Sums numbers within the range
+    let sum = 0;
+    for(let i=min; i<=max; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
