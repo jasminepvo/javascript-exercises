d7fddc48b9304fb2a9382212d812d0d5231b604c
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-05-31 22:30:52 -0400

    Working solution passing all test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    // repeats the string x many times as defined in 'num'
+    for(let i=0; i <= num; i++) {
+        let result = string.repeat(num);
+        return result;
+    }
+    // returns error when num is a negative #
+    if(num <=0)
+        return "ERROR";
+    // repeats the string random amount of times as defined in 'number'
+    for(let i=0; i <= number; i++) {
+        let result = string.repeat(number);
+        return result;
+    } 
+    // works with blank strings - additional code is not needed for this test function
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // split method splits string into array of characters, reverse method reverses array, join method joins all elements into a string
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
