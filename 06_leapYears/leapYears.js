46bb720f3bb563fee7cf8b5a9fe04c680d6876d1
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-06-01 17:20:34 -0400

    Working solution passing all 6 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Condition to check if divisible by 4 but not divisible by 100
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 ==0);
 };
 
 // Do not edit below this line
